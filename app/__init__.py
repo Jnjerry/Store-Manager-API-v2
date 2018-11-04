4c2ac63aa45e1d2577e33e51901a89bbc50f7abf
Joan Ngugi <ngugijoan2@gmail.com>
2018-11-04 21:39:34 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask,Blueprint
 from flask_jwt_extended import JWTManager
 from app.api.db.db_con import create_tables
-from app.api.db.db_con import db_connect
 
-db=db_connect()
+
+
 
 def create_app(config_name):
-    db.create_tables()
+    create_tables()
     app=Flask(__name__)
     app.config["SECRET_KEY"] = 'joanN'
     app.config['JWT_SECRET_KEY'] = 'joan'
@@ -14,4 +14,5 @@ def create_app(config_name):
     from .api.v2 import version2 as v2
     app.register_blueprint(v2)
     return app
+
 
